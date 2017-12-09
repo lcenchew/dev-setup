fed63321213c2dc9b5339bba77822370596f6fb1
Ken <ken@MBAir.local>
2017-12-09 21:00:28 +0800

    Copy folder/file separately to allow customising

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,38 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master;
+git pull origin;
 
 function doIt() {
-    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-        --exclude "README.md" --exclude "LICENSE" -avh --no-perms . ~;
-    source ~/.bash_profile;
+
+    # folders
+    cp -a .aws ~;
+    cp -a .ssh ~ && chmod 700 ~/.ssh;
+    cp -a .vim ~;
+#    cp -a bin ~;
+#    cp -a init ~;
+
+    # files
+    cp -a .aliases ~;
+    cp -a .bash_profile ~;
+    cp -a .bash_prompt ~;
+    cp -a .bashrc ~;
+    cp -a .curlrc ~;
+#    cp -a .editorconfig ~;
+    cp -a .exports ~;
+    cp -a .functions ~;
+#    cp -a .gdbinit ~;
+#    cp -a .gvimrc ~;
+#    cp -a .hgignore ~;
+    cp -a .hushlogin ~;
+    cp -a .inputrc ~;
+#    cp -a .mrjob.conf ~;
+    cp -a .s3cfg ~;
+    cp -a .screenrc ~;
+    cp -a .vimrc ~;
+    cp -a .wgetrc ~;
+
+   source ~/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
